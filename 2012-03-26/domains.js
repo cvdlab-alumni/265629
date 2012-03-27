bd8b7df627c2d589677d432c1c1d741c69d3eb28
Chiara Bicchielli <chiabic@tiscali.it>
2012-03-27 09:54:52 +0200

    domains.js

---
+++ 2012-03-26/domains.js
@@ -0,0 +1,11 @@
+//MONODIMENSIONALE
+var d1 = DOMAIN([[2,6]])([4]);
+DRAW(d1);
+
+//BIDIMENSIONALE
+var d2 = DOMAIN([[2,6],[1,4]])([4,3]);
+DRAW(d2);
+
+//TRIDIMENSIONALE
+var d3 = DOMAIN([[2,6],[1,4],[3,5]])([4,3,2]);
+DRAW(d3);
